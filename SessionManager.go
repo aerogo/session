41be80935a8a94ac9660bc05da9fe421a0118768
Eduard Urbach <e.urbach@gmail.com>
2017-06-17 20:51:13 +0200

    API changes

--- SessionManager.go
+++ SessionManager.go
@@ -3,18 +3,15 @@ package session
 // Manager ...
 type Manager struct {
 	Store Store
+
+	// Session duration in seconds
+	Duration int
 }
 
 // New creates a new session.
 func (manager *Manager) New() *Session {
-	sessionID := GenerateUUID()
-
-	// Session data is not allowed to be empty.
-	// Therefore we are adding the session ID as dummy data.
-	sessionData := map[string]interface{}{
-		"sid": sessionID,
-	}
-
+	sessionID := GenerateID()
+	sessionData := make(map[string]interface{})
 	session := New(sessionID, sessionData)
 	manager.Store.Set(session.id, session)
 	return session
--- UUID.go
+++ UUID.go
@@ -4,7 +4,15 @@ import (
 	"github.com/google/uuid"
 )
 
-// GenerateUUID generates a unique ID.
-func GenerateUUID() string {
+// sessionIDLength is a constant defining the length of session IDs.
+const sessionIDLength = 36
+
+// GenerateID generates a unique ID.
+func GenerateID() string {
 	return uuid.New().String()
+}
+
+// IsValidID returns whether the string is a valid session ID or not.
+func IsValidID(sid string) bool {
+	return len(sid) == sessionIDLength
 }
